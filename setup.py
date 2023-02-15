421b729b08f865e52864a7c1d3b91ba9b6fbdcd3
George88000 <giorgio.scarso@me.com>
2023-02-15 21:27:01 +0100

    reviewed setup.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='aircraft_models',
+    name='aircraft_models_list',
     version='1.0',
     description='This package contains a list of the Aircraft models and manufacturer as per DOC 8643 ICAO',
     author='Giorgio Scarso',
     author_email='scarso.giorgio@gmail.com',
-    py_modules=['data'],
-    package_dir={'': '.'},
-    install_requires=[]
+    packages=['aircraft_models_list'],
+    install_requires=['pandas'],
 )
