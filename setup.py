244cb92c52eb9ee33a53b308a997e7a3cf9d537d
George88000 <giorgio.scarso@me.com>
2023-02-17 10:04:22 +0100

    add right file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='aircraft_list',
-    version='2.0.1',
+    version='2.1',
     description='This package contains a list of the Aircraft models and manufacturer as per DOC 8643 ICAO',
     author='Giorgio Scarso',
     author_email='scarso.giorgio@gmail.com',
