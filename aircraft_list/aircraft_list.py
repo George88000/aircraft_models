b458dd8cb54a8f587b88a7aa34351536732d52f3
George88000 <giorgio.scarso@me.com>
2023-02-17 13:22:09 +0100

    add counter id

--- aircraft_list/aircraft_list.py
+++ aircraft_list/aircraft_list.py
@@ -6,7 +6,9 @@ def aircraft_models():
     data = []
     with open(data_file, 'r') as f:
         reader = csv.reader(f, delimiter=';')
+        counter = 0
         for row in reader:
+            counter +=1
             row_dict = {}
             for r in row:
                 row_dict['manufacturer'] = r.split(',')[0]
@@ -16,6 +18,7 @@ def aircraft_models():
                 row_dict['engine'] = r.split(',')[4]
                 row_dict['engine_number'] = r.split(',')[5]
                 row_dict['wake'] = r.split(',')[6]
+                row_dict['id'] = counter
                 data.append(row_dict)
         data.pop(0)
         return data
